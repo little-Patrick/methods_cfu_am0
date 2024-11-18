249d049b3097cf3c80534c6f55dd16575afc5b1c
Patrick Little <little.patrick@outlook.com>
2024-11-17 19:58:00 -0700

    finnished built_in_methods

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,29 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The .include? method is calling on the string "hello world"
+# I checks to see if the given string contains the Hello inside it
+# It will return a boolean value of true 
 "Hello World".include?("Hello")
 
-
+# the .end_with? method is calling on the string "Hello World"
+# It checks to see if the string ends with "Hello"
+# The string does not so it will return false
 "Hello World".end_with?("Hello")
 
-
+# the .end_with? method is calling on the string "Hello World"
+# It checks to see if the string ends with "Hello"
+# It will return a value of true
 "Hello World".end_with?("rld")
 
-
+# The method .even? is calling on the integer 12
+# It checks to see if the integer is even
+# It will return true since 12 is even
 12.even?
 
-
+# The next method is called on the integer object 18.
+# This method will return the next integer in the sequence
+# It will return a value of 19
 18.next
 
 
@@ -45,7 +55,18 @@ puts first_name.start_with?("J")
 # Declare 2 variables assigned to integer objects.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Integer.html
+
 # Include comments above each method call explaining the impact and return value of that method.
+# The method .odd? is calling on the variable which is an integer 12
+# this method will return false
+# the method that compares the variable number_1 to number_2 to see if they are equal
+# The integers that the variables represent are not equal so it will output false
+number_1 = 12
+number_2 = 4
+puts number_1.odd?
+puts number_2 == number_1
+
+
 
 
 
@@ -54,3 +75,14 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# The nums is a variable that contains the array [1,2...ect]
+# the letters is a variable that contains the array ["a","s","d","f","g","h","j","k"]
+# The method .length is going into the variable nums and counting the number of intergers(or the number of any variable whether it be a string or another) in an array
+# the output is 9 since there are 9 intergers in that array
+# The method .include?("t") looks in array that's in the variable letters and sees if there is a "t" inside that array
+# It will output false since there is no "t" in the array
+nums = [1,2,3,4,5,6,7,8,9]
+letters = ["a","s","d","f","g","h","j","k"]
+puts nums.length
+puts letters.include?("t")
